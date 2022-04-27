a54d196067f88d723e4a2cc7454619ef6a9f3805
Lucas Minetti <minettiluc95@gmail.com>
2022-04-27 20:15:59 -0300

    itemList

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import DaisyNavBar from './components/DaisyNavBar';
-import ItemListContainer from './components/ItemListContainer';
 import Contador from './components/Contador';
+import ProductosContainer from './components/Items/ProductosContainer';
+
 
 function App() {
   return (
     <div className="App">
       <DaisyNavBar></DaisyNavBar>
-      <ItemListContainer productos={"Combustibles"}></ItemListContainer>
-      <ItemListContainer productos={"Lubricantes"}></ItemListContainer>
-      <ItemListContainer productos={"Agroinsumos"}></ItemListContainer>
-      <ItemListContainer productos={"Servicios"}></ItemListContainer>
       <Contador></Contador>
+      <ProductosContainer></ProductosContainer>
     <div>
         <h1 className="text-3xl font-bold font-mono">Bienvenidos</h1>
     </div>
